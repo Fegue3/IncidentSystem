9324f8a5e0150ad7c6aa73bfeb1246f85ac25637
Francisco <fp226766@gmail.com>
2025-10-26 16:38:29 +0000

    auth dto for everything

---
+++ backend/src/auth/dto/auth.dto.ts
@@ -0,0 +1,19 @@
+import { IsEmail, IsOptional, IsString, MinLength } from 'class-validator';
+
+export class RegisterDto {
+  @IsEmail() email!: string;
+  @IsString() @MinLength(8) password!: string;
+  @IsOptional() @IsString() name?: string;
+}
+export class LoginDto {
+  @IsEmail() email!: string;
+  @IsString() password!: string;
+}
+export class ChangePasswordDto {
+  @IsString() oldPassword!: string;
+  @IsString() @MinLength(8) newPassword!: string;
+}
+export class ResetPasswordDto {
+  @IsString() token!: string;
+  @IsString() @MinLength(8) newPassword!: string;
+}
