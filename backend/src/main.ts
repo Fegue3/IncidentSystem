8f129d3a7d5def4873422e3067d7ba95be00bcc2
Francisco <fp226766@gmail.com>
2025-11-20 22:32:21 +0000

    ativei o cors no backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,8 +4,18 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // prefixo /api -> fica /api/auth/register
   app.setGlobalPrefix('api');
+  app.enableCors({
+    origin: 'http://localhost:5173',                    // onde corre o front
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',  // inclui OPTIONS (preflight)
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,                                  // se um dia usares cookies
+  });
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
   await app.listen(process.env.PORT ? Number(process.env.PORT) : 3000);
 }
 bootstrap();
