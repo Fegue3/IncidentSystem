972325723136b75ccc36e8cc8ec2222151008131
Francisco <fp226766@gmail.com>
2025-12-06 20:42:05 +0000

    add na team para a aplicação ter os modules todos

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,9 +6,10 @@ import { HealthModule } from './health/health.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { IncidentsModule } from './incidents/incidents.module';
+import { TeamsModule } from './teams/teams.module';
 
 @Module({
-  imports: [PrismaModule, HealthModule, UsersModule, AuthModule, IncidentsModule],
+  imports: [PrismaModule, HealthModule, UsersModule, AuthModule, IncidentsModule, TeamsModule],
   controllers: [AppController],
   providers: [AppService],
 })
