f8f208c665d52ba111857ff2924eac65c31b13d9
Francisco <fp226766@gmail.com>
2025-12-12 21:06:58 +0000

    update na configuração do jest

--- backend/test/config/jest.int.config.ts
+++ backend/test/config/jest.int.config.ts
@@ -2,13 +2,15 @@ import type { Config } from 'jest';
 
 const config: Config = {
   moduleFileExtensions: ['js', 'json', 'ts'],
-  rootDir: '..',
-  testRegex: 'integration/.*\\.spec\\.ts$',
-  transform: { '^.+\\.(t|j)s$': 'ts-jest' },
-  collectCoverageFrom: ['../src/**/*.ts', '!../src/main.ts'],
-  coverageDirectory: '../coverage/integration',
+  rootDir: '../..',
   testEnvironment: 'node',
-  clearMocks: true,
+  transform: { '^.+\\.(t|j)s$': 'ts-jest' },
+  testMatch: ['<rootDir>/test/integration/**/*.spec.ts'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  maxWorkers: 1,
+  collectCoverageFrom: ['<rootDir>/src/**/*.(t|j)s'],
+  coverageDirectory: '<rootDir>/coverage-integration',
+  testTimeout: 30000,
 };
 
 export default config;
