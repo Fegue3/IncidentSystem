ef1b01bd7e4ff718317d52371a761151c12e82fc
Francisco <fp226766@gmail.com>
2025-12-17 17:33:10 +0000

    será que é desta?

--- backend/src/notifications/notifications.module.ts
+++ backend/src/notifications/notifications.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
-import { IntegrationsModule } from '../integrations/integrations.module';
 @Module({
-  imports: [IntegrationsModule],
   providers: [NotificationsService],
   exports: [NotificationsService],
 })
